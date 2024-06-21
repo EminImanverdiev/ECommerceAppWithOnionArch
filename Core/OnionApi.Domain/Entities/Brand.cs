a2a073f9dd10c2ec3af42b6fa020e516615a0a0b
elmin007 <imanverdiyevemin414@gmail.com>
2024-06-21 16:05:58 +0400

    Created main entities

---
+++ Core/OnionApi.Domain/Entities/Brand.cs
@@ -0,0 +1,20 @@
+﻿using OnionApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionApi.Domain.Entities
+{
+    public class Brand:EntityBase
+    {
+        public Brand() { }  
+        public Brand(string name)
+        {
+            this.Name = name;
+        }
+        public required string Name { get; set; }
+
+    }
+}
---
+++ Core/OnionApi.Domain/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using OnionApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionApi.Domain.Entities
+{
+    public class Category:EntityBase
+    {
+        public Category(){}
+        public Category(int parentId,string name,int priorty)
+        {
+          this.ParentId = parentId;
+          this.Name = name;
+          this.Priorty = priorty;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
