ac4703bf8a5d49929d0917001cc95dbb90cd295d
elmin007 <imanverdiyevemin414@gmail.com>
2024-06-25 14:03:22 +0400

    Added Automapper and installed

---
+++ OnionApi.Mapper/AutoMapper/Mapper.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.Internal;
+
+namespace OnionApi.Mapper.AutoMapper
+{
+    public class Mapper : Application.Interfaces.AutoMapper.IMapper
+    {
+        public static List<TypePair> typePairs = new();
+        private IMapper MapperContainer;
+        public TDestination Map<TDestination, TSource>(TSource source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TDestination> Map<TDestination, TSource>(IList<TSource> sources, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TDestination Map<TDestination, TSource>(object source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TDestination> Map<TDestination, TSource>(IList<object> source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
