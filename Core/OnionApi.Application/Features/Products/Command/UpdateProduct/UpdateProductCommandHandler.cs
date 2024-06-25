d1ccdf4b4b0fb1a36a75fdd9fe706287fe51cb7e
elmin007 <imanverdiyevemin414@gmail.com>
2024-06-26 01:26:29 +0400

    Created Update folder on the project

---
+++ Core/OnionApi.Application/Features/Products/Command/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionApi.Application.Features.Products.Command.UpdateProduct
+{
+    public class UpdateProductCommandHandler
+    {
+    }
+}
