27d2430c2fb5fd351a05dc9f01f522cae569815b
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-15 15:27:12 +0400

    done Redis Connection, RedisService on the project

---
+++ Core/OnionApi.Application/Beheviors/RedisCacheBehevior.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using OnionApi.Application.Interfaces.RedisCache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionApi.Application.Beheviors
+{
+    public class RedisCacheBehevior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService _redisCacheService;
+
+        public RedisCacheBehevior(IRedisCacheService redisCacheService)
+        {
+            _redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query) {
+                var cacheKey= query.CacheKey;
+                var cacheTime= query.CacheTime;
+
+                var cacheData = await _redisCacheService.GetAsync<TResponse>(cacheKey);
+                if (cacheData is not null) return cacheData;
+                var response = await next();
+                if (response is not null) await _redisCacheService.SetAsync(cacheKey, response,DateTime.UtcNow.AddMinutes(cacheTime));
+                return response;
+            }
+            return await next(); 
+        }
+    }
+}
---
+++ Infrastructure/OnionApi.Infrastructure/RedisCache/RedisCacheService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OnionApi.Application.Interfaces.RedisCache;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionApi.Infrastructure.RedisCache
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private readonly IDatabase _database;
+        private readonly RedisCacheSettings _settings;
+        public RedisCacheService(IOptions<RedisCacheSettings> options)
+        {
+            _settings = options.Value;
+            var opt = ConfigurationOptions.Parse(_settings.ConnectionString);
+            _connectionMultiplexer=ConnectionMultiplexer.Connect(opt);
+            _database = _connectionMultiplexer.GetDatabase();
+        }
+        public async Task<T> GetAsync<T>(string key)
+        {
+            var value=await _database.StringGetAsync(key);
+            if (value.HasValue)
+                return JsonConvert.DeserializeObject<T>(value);
+            return default;
+        }
+
+        public async Task<T> SetAsync<T>(string key, T value, DateTime? expirationTime = null)
+        {
+            TimeSpan timeSpan=expirationTime.Value-DateTime.UtcNow;
+            await _database.StringSetAsync(key,JsonConvert.SerializeObject(value),timeSpan);
+        }
+    }
+}
